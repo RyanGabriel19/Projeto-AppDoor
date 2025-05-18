d99988d0964b08fd18fe78d04d9de7c03ccefc64
RyanGabriel19 <ryangabriel.apf19@gmail.com>
2025-05-18 18:37:03 -0300

    adicionando o cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import logRoutes from './routes/logRoutes.js'
 import movPortaRoutes from './routes/movPortaRoutes.js'
@@ -6,6 +7,7 @@ import movPortaRoutes from './routes/movPortaRoutes.js'
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use(usuarioRoutes, logRoutes, movPortaRoutes);
 
