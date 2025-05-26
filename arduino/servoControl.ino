802a190732c987b10b497460ec23b529abd337bf
RyanGabriel19 <ryangabriel.apf19@gmail.com>
2025-05-26 20:46:15 -0300

    Trazendo o arduino

---
+++ arduino/servoControl.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+Servo servo;
+SoftwareSerial BT(10, 11); // RX, TX
+
+void setup() {
+  servo.attach(9);
+  BT.begin(9600);
+}
+
+void loop() {
+  if (BT.available()) {
+    char c = BT.read();
+    if (c == 'A')      servo.write(90);
+    else if (c == 'F') servo.write(0);
+  }
+}
